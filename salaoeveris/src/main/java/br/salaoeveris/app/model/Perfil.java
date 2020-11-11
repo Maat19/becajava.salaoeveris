bbb68bacbfd77415085f7ccade1781234b4967b5
Mateus Maia Agre <mmaiaagr@everis.com>
2020-11-11 17:13:38 -0300

    terceiro commit

--- salaoeveris/src/main/java/br/salaoeveris/app/model/Perfil.java
+++ salaoeveris/src/main/java/br/salaoeveris/app/model/Perfil.java
@@ -1,41 +1,41 @@
-package br.salaoeveris.app.model;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-import org.springframework.security.core.GrantedAuthority;
-
-@Entity
-public class Perfil implements GrantedAuthority {
-
-	
-	
-	
-	@Id 
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	
-	private static final long serialVersionUID = 1L;
-	private String nome;
-
-	
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-
-	@Override
-	public String getAuthority() {
-		return nome;
-	}
-	
-	
-}
+//package br.salaoeveris.app.model;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//import org.springframework.security.core.GrantedAuthority;
+//
+//@Entity
+//public class Perfil implements GrantedAuthority {
+//
+//	
+//	
+//	
+//	@Id 
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private Long id;
+//	
+//	
+//	private static final long serialVersionUID = 1L;
+//	private String nome;
+//
+//	
+//
+//	public String getNome() {
+//		return nome;
+//	}
+//
+//	public void setNome(String nome) {
+//		this.nome = nome;
+//	}
+//	
+//
+//	@Override
+//	public String getAuthority() {
+//		return nome;
+//	}
+//	
+//	
+//}
--- salaoeveris/src/main/java/br/salaoeveris/app/service/TokenService.java
+++ salaoeveris/src/main/java/br/salaoeveris/app/service/TokenService.java
@@ -1,32 +1,32 @@
-package br.salaoeveris.app.service;
-
-import java.util.Date;
-
-import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Service;
-
-import br.salaoeveris.app.model.Usuario;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-
-@Service
-public class TokenService {
-	
-
-	public String gerarToken(Authentication authentication) {
-		Usuario logado = (Usuario) authentication.getPrincipal();
-		
-		Date hoje = new Date();
-		Date dataExpiracao = new Date(hoje.getTime() + 86400000);
-		String secret = "tercessireveoalasetset";
-		
-		return Jwts.builder()
-				.setId("API Salão Everis")
-				.setSubject(logado.getId().toString())
-				.setIssuedAt(hoje)
-				.setExpiration(dataExpiracao)
-				.signWith(SignatureAlgorithm.HS256, secret)
-				.compact();
-	}
-
-}
+//package br.salaoeveris.app.service;
+//
+//import java.util.Date;
+//
+//import org.springframework.security.core.Authentication;
+//import org.springframework.stereotype.Service;
+//
+//import br.salaoeveris.app.model.Usuario;
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//
+//@Service
+//public class TokenService {
+//	
+//
+//	public String gerarToken(Authentication authentication) {
+//		Usuario logado = (Usuario) authentication.getPrincipal();
+//		
+//		Date hoje = new Date();
+//		Date dataExpiracao = new Date(hoje.getTime() + 86400000);
+//		String secret = "tercessireveoalasetset";
+//		
+//		return Jwts.builder()
+//				.setId("API Salão Everis")
+//				.setSubject(logado.getId().toString())
+//				.setIssuedAt(hoje)
+//				.setExpiration(dataExpiracao)
+//				.signWith(SignatureAlgorithm.HS256, secret)
+//				.compact();
+//	}
+//
+//}
